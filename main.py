88289a8aa9b5d22e30e088cbff96ff625c122d4d
vowelphobic <178687109+dnljns@users.noreply.github.com>
2026-01-26 21:56:13 -0500

    Reformat with `black`

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from sys import argv
 
-operations = {
-    "/": "not",
-    "*": "and",
-    "+": "or"
-}
+operations = {"/": "not", "*": "and", "+": "or"}
 
 if __name__ == "__main__":
     if len(argv) > 1:
         with open(argv[1], "r") as file:
-            equations = [ equation for equation in file.readlines() if not equation.isspace() ]
+            equations = [
+                equation for equation in file.readlines() if not equation.isspace()
+            ]
     else:
-        equations = [ input("Input your logic equation:\n> "), ]
-    equations = [ equation.strip().replace(" ", "") for equation in equations ]
+        equations = [
+            input("Input your logic equation:\n> "),
+        ]
+    equations = [equation.strip().replace(" ", "") for equation in equations]
 
     for equation in equations:
         print(equation)
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
         for char in equation:
             if char == equation[0]:
-                continue # Skip output pin
+                continue  # Skip output pin
             if char.isalpha() and char not in pin_mapping:
                 pin_mapping[char] = pin_count
                 pin_count += 1
@@ -33,6 +33,6 @@ if __name__ == "__main__":
         for pin_voltages in range(2**pin_count):
             for var in pin_mapping:
                 globals()[var] = (pin_voltages >> pin_mapping[var]) & 1
-                print(f"{var}: {globals()[var]}", end = " ")
+                print(f"{var}: {globals()[var]}", end=" ")
             output = int(eval(equation[2:]))
             print(f"{equation[0]}: {output}")
