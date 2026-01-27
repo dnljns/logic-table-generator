f6f407b201a4e20e2fd8a05e64f4781b884966f9
vowelphobic <178687109+dnljns@users.noreply.github.com>
2026-01-27 17:18:49 -0500

    Legally negative!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sys import argv
+from pick import pick
 
 operations = {"/": "not", "*": "and", "+": "or"}
 
@@ -22,17 +23,28 @@ if __name__ == "__main__":
 
         for char in equation:
             if char == equation[0]:
+                output_pin = equation[0]
                 continue  # Skip output pin
             if char.isalpha() and char not in pin_mapping:
                 pin_mapping[char] = pin_count
                 pin_count += 1
+        
+        selected = pick(list(pin_mapping.keys()) + [output_pin], "Select all active-low pins:", indicator="=>", multiselect=True)
+        for pin in selected:
+            if pin[0] == output_pin:
+                equation = f"{pin[0]}=not ({equation[2:]})"
+            else:
+                equation = equation.replace(pin[0], f"(/{pin[0]})")
 
         for symbol in operations:
             equation = equation.replace(symbol, " %s " % operations[symbol])
 
+        print()
+        print(" ".join(list(pin_mapping.keys())), "|", output_pin)
+        print("-"*(len(pin_mapping)*2+3))
         for pin_voltages in range(2**pin_count):
             for var in pin_mapping:
                 globals()[var] = (pin_voltages >> pin_mapping[var]) & 1
-                print(f"{var}: {globals()[var]}", end=" ")
+                print(globals()[var], end=" ")
             output = int(eval(equation[2:]))
-            print(f"{equation[0]}: {output}")
+            print("|", output)
